7117e433c40ef0dec8f694d42982d971920ba2eb
msa-39 <SergeyA.Moiseev39@gmail.com>
2019-08-26 11:19:00 +0300

    Commit 2019/07/26 11:12

--- cus.py
+++ cus.py
@@ -7,11 +7,11 @@ from utills import clients
 import json
 import logging
 
-logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s', level=logging.DEBUG, filename=u'abs_api_cus.log')
+#logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s', level=logging.INFO, filename=u'abs_api_cus.log')
 
 app = Flask(__name__)
 
-#app.config['JSON_SORT_KEYS'] = False
+app.config['JSON_SORT_KEYS'] = False
 
 auth = HTTPTokenAuth('Bearer')
 
@@ -25,7 +25,7 @@ def unauthorized():
     # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
 
 @app.errorhandler(400)
-def not_found(error):
+def bad_request(error):
     return make_response(jsonify({'error': 'Bad request'}), 400)
 
 @app.errorhandler(404)
@@ -33,7 +33,7 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('/absapi/v1/cus', methods=['GET'])
-#@auth.login_required
+@auth.login_required
 # Get short information about ALL Custimers, hwo has opened account
 def GetCusAll():
     inn = request.args.get('inn')
@@ -47,6 +47,7 @@ def GetCusAll():
     plSQL = "select isb_abs_api_util.pljson_value2clob(isb_abs_api_cus.get_all_cus_info(:inn)) from dual"
     cu.execute(plSQL, inn=inn)
     res=cu.fetchall()
+    #if res
     return make_response(jsonify(json.loads(res[0][0].read())), 200)
 
 @app.route('/absapi/v1/cus/<int:icusnum>', methods=['GET'])
@@ -212,6 +213,22 @@ def GetCusDepositsInfo(icusnum):
     res=cu.fetchall()
     return make_response(jsonify(json.loads(res[0][0].read())), 200)
 
+@app.route('/absapi/v1/cus/<int:icusnum>/rentdogs', methods=['GET'])
+#@auth.login_required
+# Get information about Custimer Rent Contracts
+def GetCusRentdogsInfo(icusnum):
+    res = None
+    con = None
+    try:
+        con = absdb.set_connection()
+    except:
+        print('[ERROR] DB Connection ERROR!')
+    cu = con.cursor()
+    plSQL = "select isb_abs_api_util.pljson_value2clob(isb_abs_api_cus.get_cus_rentdogs_info(:icusnum)) from dual"
+    cu.execute(plSQL, icusnum=icusnum)
+    res=cu.fetchall()
+    return make_response(jsonify(json.loads(res[0][0].read())), 200)
+
 ########################################################################################################################
 
 #@app.route('/absapi/v1/cus_full/', methods=['GET'])
@@ -230,4 +247,4 @@ def GetCusDepositsInfo(icusnum):
 
 if __name__ == '__main__':
     app.run(ssl_context=('/home/moiseev/cert.pem', '/home/moiseev/key.pem'),
-            debug=True, host='0.0.0.0', port=5002)
+            debug=True, host='0.0.0.0', port=5000)
