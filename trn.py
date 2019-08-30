24f035b7ad68e1aca8a25f9191e4fffe23022db5
msa-39 <SergeyA.Moiseev39@gmail.com>
2019-08-30 19:42:35 +0300

    Commit 2019/08/30 19:35

--- trn.py
+++ trn.py
@@ -1,14 +1,36 @@
 # Микросервис API для работы с проводками
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, make_response, request
+import json
 from dbutills import absdb
+from flask_httpauth import HTTPTokenAuth
 
 app = Flask(__name__)
+
+app.config['JSON_SORT_KEYS'] = False
+
+auth = HTTPTokenAuth('Bearer')
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'ERROR': 'Unauthorized access'}), 403)
+    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'ERROR': 'Bad request'}), 400)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'ERROR': 'Not found'}), 404)
+
 
 @app.route('/absapi/v1/trn', methods=['POST'])
 # Register PayMent
 def RegPayment():
     return make_response("[Ok] RegPayment\n", 200)
+
+
 
 
 if __name__ == '__main__':
