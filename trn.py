c457cdce641e6f799b50154fb384d1e176fc6b44
msa-39 <SergeyA.Moiseev39@gmail.com>
2019-09-06 19:28:38 +0300

    Commit 2019/09/06 19:21

--- trn.py
+++ trn.py
@@ -26,24 +26,40 @@ def not_found(error):
     return make_response(jsonify({'ERROR': 'Not found'}), 404)
 
 
-@app.route('/absapi/v1/trn/document/<string:itrnnum_itrnanum>', methods=['GET'])
+@app.route('/absapi/v1/document/<string:p_docid>', methods=['GET'])
 # Get document (transaction) information
 
 #@auth.login_required
-def GetTrnInfo(itrnnum_itrnanum):
+def GetTrnInfo(p_docid):
 
-    l_itrnnum = int(re.match(r'\d+', itrnnum_itrnanum).group(0))
-    l_itrnanum = int(re.sub(r'\d+_', '', itrnnum_itrnanum))
+    try:
+        l_doctype = re.match(r'((t|T)(r|R)(n|N)|(t|T)(r|R)(c|C)|(d|D)(p|P)(d|D))', p_docid).group(0).upper()
+        p_docid   =   re.sub(r'((t|T)(r|R)(n|N)|(t|T)(r|R)(c|C)|(d|D)(p|P)(d|D))_', '', p_docid)
+        l_inum = int(re.match(r'\d+', p_docid).group(0))
+        l_ianum = int(re.sub(r'\d+_', '', p_docid))
+
+    except:
+        return bad_request(400)
 
     try:
         con = absdb.set_connection()
     except:
         print('[ERROR] DB Connection ERROR!')
     cu = con.cursor()
-    plSQL = "select isb_abs_api_util.pljson_value2clob(isb_abs_api_trn.get_trn_info(:p_itrnnum, :p_itrnanum)) from dual"
-    cu.execute(plSQL, p_itrnnum=l_itrnnum, p_itrnanum=l_itrnanum)
+    plSQL = ''
+
+    if re.fullmatch(r'TRN', l_doctype):
+        plSQL = "select isb_abs_api_util.pljson_value2clob(isb_abs_api_trn.get_trn_info(:p_itrnnum, :p_itrnanum)) from dual"
+
+    if (len(plSQL) == 0):
+        return bad_request(400)
+
+    cu.execute(plSQL, p_itrnnum=l_inum, p_itrnanum=l_ianum)
     res=cu.fetchall()
     return make_response(jsonify(json.loads(res[0][0].read())), 200)
+
+#    print(plSQL)
+#    return "l_doctype = "+l_doctype+"</br> l_inum = "+str(l_inum)+"</br> l_ianum = "+str(l_ianum)
 
 
 @app.route('/absapi/v1/trn', methods=['POST'])
@@ -56,4 +72,4 @@ def RegPayment():
 
 if __name__ == '__main__':
     app.run(ssl_context=('/home/moiseev/cert.pem', '/home/moiseev/key.pem'),
-            debug=True, host='0.0.0.0', port=5000)
+            debug=True, host='0.0.0.0', port=5003)
